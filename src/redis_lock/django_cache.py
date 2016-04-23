df4f6219fe91e8fcadf2c37de8f7ecca996ad01b
athre0z <athre0z@mainframe.pw>
2016-04-23 23:40:16 +0200

    Added convenience method "get_or_set_locked"

--- src/redis_lock/django_cache.py
+++ src/redis_lock/django_cache.py
@@ -1,3 +1,4 @@
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django_redis.cache import RedisCache as PlainRedisCache
 
 from redis_lock import Lock
@@ -18,6 +19,36 @@ class RedisCache(PlainRedisCache):
 
     def lock(self, key, expire=None, id=None):
         return Lock(self.__client, key, expire=expire, id=id)
+
+    def get_or_set_locked(self, key, value_creator, version=None,
+                          expire=None, id=None, lock_key=None,
+                          timeout=DEFAULT_TIMEOUT):
+        """
+        Fetch a given key from the cache. If the key does not exist, the key is added and
+        set to the value returned when calling `value_creator`. The creator function
+        is invoked inside of a lock.
+        """
+        # As users might put `None` into the cache, we need something
+        # uniquely recognizable for our `default` argument to `.get`.
+        not_set = object()
+
+        if lock_key is None:
+            lock_key = '{0}.set-lock'.format(key)
+
+        val = self.get(key, version=version, default=not_set)
+        if val is not not_set:
+            return val
+        else:
+            with self.lock(lock_key, expire=expire, id=id):
+                # Was the value set while we were trying to acquire the lock?
+                val = self.get(key, version=version, default=not_set)
+                if val is not not_set:
+                    return val
+
+                # Nope, create value now.
+                val = value_creator()
+                self.set(key, val, timeout=timeout, version=version)
+                return val
 
     def reset_all(self):
         """
