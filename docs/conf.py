0b9ca437a3a7168a3cbcdc343b5dc79f63d0a71c
SalomonSmeke <ssmeke@luc.edu>
2020-11-16 12:32:06 -0600

    Docs: fix broken URL's, ignore a couple in sphynx cfg.

--- docs/conf.py
+++ docs/conf.py
@@ -22,13 +22,17 @@ author = 'Ionel Cristian Mărieș'
 copyright = '{0}, {1}'.format(year, author)
 version = release = '3.6.0'
 
+linkcheck_ignore = [
+    'https://coveralls\\.io/repos/ionelmc/python\\-redis\\-lock/badge\\.svg\\?branch=master\\&service=github',  # This redirects to hosted % image on AWS.
+    'https://github\\.com/jazzband/django\\-redis\\#configure\\-as\\-cache\\-backend',  # This anchor is incorrectly marked as missing. GH bug?
+]
 pygments_style = 'trac'
 templates_path = ['.']
 extlinks = {
     'issue': ('https://github.com/ionelmc/python-redis-lock/issues/%s', '#'),
     'pr': ('https://github.com/ionelmc/python-redis-lock/pull/%s', 'PR #'),
 }
-html_theme = "sphinx_py3doc_enhanced_theme"
+html_theme = 'sphinx_py3doc_enhanced_theme'
 html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
 html_theme_options = {
     'githuburl': 'https://github.com/ionelmc/python-redis-lock/'
