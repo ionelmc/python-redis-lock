a9ae3ef6bd544e41cc46535af98c704cdb877756
Ionel Cristian Mărieș <contact@ionelmc.ro>
2022-10-17 15:55:10 +0300

    Up skel. Drop py3.6.

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -67,7 +66,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'redis>=2.10.0',
     ],
